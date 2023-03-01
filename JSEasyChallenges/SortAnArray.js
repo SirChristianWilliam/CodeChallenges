4dac62f6c0f1602fca931c72392eea4fd3566f68
Chris Maki <c.william.developer@gmail.com>
2023-03-01 16:32:11 -0600

    Solved SortAnArray

---
+++ JSEasyChallenges/SortAnArray.js
@@ -0,0 +1,37 @@
+// 912. Sort an Array
+// Medium
+// 4.4K
+// 662
+// Companies
+// Given an array of integers nums, sort the array in ascending order and return it.
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+// Example 1:
+
+// Input: nums = [5,2,3,1]
+// Output: [1,2,3,5]
+// Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+// Example 2:
+
+// Input: nums = [5,1,1,2,0,0]
+// Output: [0,0,1,1,2,5]
+// Explanation: Note that the values of nums are not necessairly unique.
+
+// Constraints:
+
+// 1 <= nums.length <= 5 * 104
+// -5 * 104 <= nums[i] <= 5 * 104
+
+// My answer:
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function (nums) {
+  return nums.sort(function (a, b) {
+    return a - b;
+  });
+};
+
+console.log(sortArray([4, 2, 0, 1, 0, -4, -55, -105, 223]));
